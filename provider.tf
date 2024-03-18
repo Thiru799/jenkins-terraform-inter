2c48c989641792e1d4bda8ca00803d90b8acd439
Thiru799 <147150748+Thiru799@users.noreply.github.com>
2024-03-18 16:38:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   # Configuration options
   region = "ap-south-1"
-  access_key =  "AKIAVIOCP3LDSVM4RVFD"
-  secret_key = "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"
+  access_key =  "AKIAVIOCP3LD4FBILDFO"
+  secret_key = "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"
 }
